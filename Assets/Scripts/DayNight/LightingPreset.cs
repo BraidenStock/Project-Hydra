ddea39924dfd3ad3a79cfaf5bebf541b71ba825f
BraidenStock <stockbraiden@gmail.com>
2026-03-21 17:03:11 -0600

    Environment Work
    New beach environment with textures. Animated water shader for ocean and added day/night cycle(still needs tuning).

---
+++ Assets/Scripts/DayNight/LightingPreset.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "ScriptableObjects/Lighting Preset", order = 1)]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient ambientColor;
+    public Gradient directionalColor;
+    public Gradient fogColor;
+}
